32d9482192e01c77bc0a9d329217a25bebdb55dd
Dongkeun Lee <ldkz2524@ucla.edu>
2014-04-06 00:08:18 -0700

    with sample data

--- creativity.py
+++ creativity.py
@@ -25,7 +25,7 @@ from bs4 import BeautifulSoup
 from collections import deque
 from collections import Counter
 from suds.client import Client
-import csv, codecs, cStringIO
+import csv
 import logging
 import operator
 import time
@@ -47,12 +47,15 @@ class Authenticate (object):
 		self.SID = None
 	def authenticateSession (self):
 		try:
+			time.sleep(0.5)
 			self.SID = self.client.service.authenticate()
 		except suds.WebFault, e:
 			print e
 			sys.exit()	
 	def closeSession (self):
-		try:
+		try:	
+			time.sleep(0.5)
+			self.SID = self.client.service.authenticate()
 			self.client.service.closeSession()
 		except suds.WebFault, e:
 			print e
@@ -63,21 +66,21 @@ class Query (object):
 		self.client = Client(queryUrl)
 		self.client.set_options(headers={'Cookie':"SID=\""+str(SID)+"\""})
 	def search (self, queryParameters, retrieveParameters):
-		time.sleep(1)
+		time.sleep(0.5)
 		try:
 			result = self.client.service.search(queryParameters,retrieveParameters)
 			return result
 		except suds.WebFault, e:
 			print e
 	def citingArticles (self, databaseId, uid, editions, timeSpan, language, retrieveParameters):
-		time.sleep(1)
+		time.sleep(0.5)
 		try:
 			result = self.client.service.citingArticles( databaseId, uid, editions, timeSpan, language, retrieveParameters)
 			return result
 		except suds.WebFault, e:
 			print e
 	def citedReferences (self, databaseId, uid, language, retrieveParameters):
-		time.sleep(1)
+		time.sleep(0.5)
 		try:
 			result = self.client.service.citedReferences(databaseId,uid,language,retrieveParameters)
 			return result
@@ -187,35 +190,6 @@ def duplicate_reference (pair_list):
 		last = x
 		new_list.append(last)
 	return new_list
-
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
 
 #--------------------
 #MAIN STARTS HERE
@@ -354,14 +328,14 @@ def main(argv):
 	alc_sorted = sorted(alc.items(),key=lambda(k,v):(-v,k))
 
 	with open(outputfile2+".csv",'ab') as f:
-		writer = UnicodeWriter(f, quoting=csv.QUOTE_NONNUMERIC)
+		writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
 		for key, value in alc_sorted:
-			writer.writerow([str(key),str(value)])
+			writer.writerow([key,value])
 
 	with open(outputfile1+".csv",'ab') as g:
-		writer1 = UnicodeWriter(g, quoting=csv.QUOTE_NONNUMERIC)
+		writer1 = csv.writer(g, quoting=csv.QUOTE_NONNUMERIC)
 		for row in total_list:
-			writer1.writerow([row['input'],row['output'],str(row['count'])])
+			writer1.writerow([row['input'],row['output'],row['count']])
 	
 if __name__ == "__main__":
 	main(sys.argv[1:])
